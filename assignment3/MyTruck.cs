2ce2e67ae92b3a2a0947abc5347ed08b0a412841
Lauren <laurenleighpetrus3@gmaiil.com>
2023-12-08 13:42:38 +0200

    first commit

---
+++ assignment3/MyTruck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+   internal class MyTruck
+    {
+        private  string whatbrand;
+        private int speed;
+        private int NumberOfDoor;
+        private int NumberOfWheels;
+
+       public MyTruck (string whatBrand,int Speed, int numberOfDoors,int numberOfWheels) 
+       {
+            whatbrand = whatBrand;
+            speed = Speed;
+            NumberOfDoor = numberOfDoors;   
+            NumberOfWheels = numberOfWheels;
+        
+       }
+        public string GetBrand()
+        { 
+            return whatbrand; 
+        }
+
+        public int GetSpeed()
+        { return speed; }
+
+        public int GetNumberOfDoor()
+        { return NumberOfDoor; }
+
+        public int GetNumberOfWheels() 
+        {  return NumberOfWheels; }
+    }
+}
---
+++ assignment3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTruck Fred = new MyTruck("bmw",100,4,5);
+           
+            Console.WriteLine("Car Brand: " + Fred.GetBrand());
+            Console.WriteLine("How fast it drives:" + Fred.GetSpeed());
+            Console.WriteLine("Number of wheels the truck has:" + Fred.GetNumberOfWheels());
+            Console.WriteLine("Number of doors the truck has:" + Fred.GetNumberOfDoor());
+        }
+    }
+}
